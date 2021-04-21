be3062f84a8856b24f4b4798ae0d77d0d6261581
Lucas Estevam <tl.lucasestevam@gmail.com>
2021-04-21 15:50:19 -0300

    Day 03 | Consuming the api to list the podscast and adding the statistical pages for each episode

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,7 @@
 import styles from './styles.module.scss'
 import format from 'date-fns/format'
 import ptBR from 'date-fns/locale/pt-BR'
+import Link from "next/link";
 
 export function Header() {
   const currentDate = format(new Date(), 'EEEEEE, d MMMM', {
@@ -9,7 +10,9 @@ export function Header() {
 
   return (
     <header className={styles.headerContainer}>
-      <img src="/logo.svg" alt="Podcastr"/>
+      <Link href="/">
+        <img className={styles.logo} src="/logo.svg" alt="Podcastr"/>
+      </Link>
       <p>O melhor para você ouvir, sempre</p>
 
       <span>{currentDate}</span>
