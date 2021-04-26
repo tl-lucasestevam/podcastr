413c63472052d9f798fc71b6080e93a0365d9d4a
Lucas Estevam <tl.lucasestevam@gmail.com>
2021-04-26 13:59:20 -0300

    feat: adding friendly commit
    Adding friendly commit using husk, commitizen and commitlint

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,19 @@ import { Player } from '../components/Player'
 
 import styles from '../styles/app.module.scss'
 
+import ProgressBar from "@badrap/bar-of-progress";
+import Router from "next/router";
+
+const progress = new ProgressBar({
+  size: 5,
+  color: "#04D361",
+  className: "bar-of-progress"
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
+
 function MyApp({ Component, pageProps }) {
   return (
     <div className={styles.wrapper}>
